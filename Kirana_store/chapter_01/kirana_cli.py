14b1d216e1c3755c3d6b911923ae4f816dc2124e
Rashmi Chaudhari <rashmichoudhari1997@gmail.com>
2026-01-06 23:02:21 +0530

    update the comment

--- Kirana_store/chapter_01/kirana_cli.py
+++ Kirana_store/chapter_01/kirana_cli.py
@@ -1,5 +1,5 @@
 #--------------------Kirana Store CLI-------------------
-#Stock List
+#Stock List of products
 stock = [
     {"name": "Rice", "price": 60, "quantity": 50},
     {"name": "Wheat", "price": 45, "quantity": 60},
